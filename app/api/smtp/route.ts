56f92caec0710a1a4871f9953914d87d4ab86268
Jofer Usa <joferusa43@gmail.com>
2025-03-04 13:06:45 +0800

    Implement SMTP API route and Resend

--- app/api/smtp/route.ts
+++ app/api/smtp/route.ts
@@ -1,5 +1,21 @@
-export async function POST(req: Request) {
-	return new Response("Email sent", {
-		status: 200,
+import { NextApiRequest, NextApiResponse } from "next";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+	const {from, to, subject, text} = req.body()
+	
+	const { data, error } = await resend.emails.send({
+		from: from,
+		to: to,
+		subject: subject,
+		text: text,
 	});
+
+	if (error) {
+		return res.status(400).json(error);
+	}
+
+	res.status(200).json(data);
 }
